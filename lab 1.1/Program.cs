f4c099b7441d17c73d6cb8fa54a98e2996f311df
arXaik <araik200414@gmail.com>
2022-11-10 00:14:50 +0200

    Добавьте файлы проекта.

---
+++ lab 1.1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Петросян Араик 22-Исп-2/1 средний уровень 1.1
+Console.WriteLine("Введите t:");
+double t = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите r:");
+double r = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите y:");
+double y = double.Parse(Console.ReadLine());
+double part1 = 4 * (t * t * t) + Math.Log(r);
+double part2 = Math.Pow(Math.Exp(1), y + r) + 7.2 * Math.Sin(r);
+double W = part1 / part2;
+Console.WriteLine($"W={W}");
